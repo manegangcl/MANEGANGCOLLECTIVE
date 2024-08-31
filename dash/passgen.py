5ea00a34fdd1a27136372d9c7372b050d5c491c9
PDFSA <privatedrumkitfoldersyncapp@gmail.com>
2024-08-31 18:15:18 -0500

    infom

--- dash/passgen.py
+++ dash/passgen.py
@@ -1,4 +1,4 @@
 import secrets
 import string
 
-print('\n'.join(''.join(secrets.choice("abcdefghijklmnopqrstuvwxyz" + "1234567890") for _ in range(14)) for _ in range(20)))
+print('\n'.join(''.join(secrets.choice("abcdefghijklmnopqrstuvwxyz" + "1234567890") for _ in range(14)) for _ in range(10)))
